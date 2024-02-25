d3e80f3a150d44c7981c859e152a5e855db2f36d
Ezequiel Escobar <escosou@gmail.com>
2024-02-24 21:05:18 -0300

    Add BusinessCard

--- app/src/main/java/com/example/androidcomposepratice/MainActivity.kt
+++ app/src/main/java/com/example/androidcomposepratice/MainActivity.kt
@@ -3,16 +3,23 @@ package com.example.androidcomposepratice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting()
+                    BusinessCard()
                 }
             }
         }
@@ -38,31 +45,94 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting() {
+fun BusinessCard() {
+    val background = painterResource(R.drawable.bg_businesscard)
+
+    Box {
+        Image(
+            painter = background,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+
+        Greeting(
+            modifier = Modifier
+                .padding(8.dp)
+        )
+        Contact()
+
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ph_devcard)
     val developer: String = stringResource(R.string.developer)
     val description: String = stringResource(R.string.dev_description)
+
     Column(
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+//            .fillMaxSize(1f)
+            .padding(16.dp)
     ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            Modifier
+                .width(150.dp)
+                .align(Alignment.CenterHorizontally)
+        )
         Text(
             text = developer,
-            fontSize = 16.sp,
+            fontSize = 40.sp,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 0.dp)
+            color = Color.White,
+            modifier = Modifier
+                .padding(16.dp, 16.dp, 16.dp, 0.dp)
+                .background(color = Color.Black)
+                .align(Alignment.CenterHorizontally)
         )
         Text(
             text = description,
-            fontSize = 11.sp,
-            modifier = Modifier.padding(4.dp)
+            color = Color.White,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(1.dp)
+                .background(color = Color.Black)
+                .align(Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun Contact(modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = stringResource(id = R.string.phone),
+            color = Color.White,
+            modifier = Modifier
+                .background(color = Color.Black)
+        )
+        Text(
+            text = stringResource(id = R.string.github),
+            color = Color.White,
+            modifier = Modifier
+                .background(color = Color.Black)
+        )
+        Text(
+            text = stringResource(id = R.string.email),
+            color = Color.White,
+            modifier = Modifier
+                .background(color = Color.Black)
         )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BusinessCardPreview() {
     AndroidComposePraticeTheme {
-        Greeting()
+        BusinessCard()
     }
 }
